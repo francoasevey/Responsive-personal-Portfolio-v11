9f5088cfbba86b2f9600dcc420e81e4b853fb1cc
francoromeo <francoromeo5012@gmail.com>
2024-01-03 11:34:37 -0300

    testimonial swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,13 @@ let swiperProjects = new Swiper(".projects-container", {
 });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiperTestimonial = new Swiper(".testimonial-container", {
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 /*=============== EMAIL JS ===============*/
 
