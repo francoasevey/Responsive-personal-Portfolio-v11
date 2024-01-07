66f8ddb3a027da643be2d79ab9547f781ed9c1d7
francoromeo <francoromeo5012@gmail.com>
2024-01-06 23:16:38 -0300

    media big

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,7 +147,12 @@ themeButton.addEventListener('click', () =>{
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+    : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
